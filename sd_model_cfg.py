4e90605aeefec1525d7f8918c223ea0b143a4530
Shuai Yang <596836482@qq.com>
2023-06-08 20:40:31 +0800

    Update sd_model_cfg.py

--- sd_model_cfg.py
+++ sd_model_cfg.py
@@ -2,5 +2,6 @@
 
 model_dict = {
     'Stable Diffusion 1.5': '',
-    'revAnimated_v11': 'models/revAnimated_v11.safetensors'
+    'revAnimated_v11': 'models/revAnimated_v11.safetensors',
+    'realisticVisionV20_v20': 'models/realisticVisionV20_v20.safetensors'
 }
